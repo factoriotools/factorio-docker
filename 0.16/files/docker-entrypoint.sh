90bce8d38a0624edf2be4b39cd66757e775ccb1c
Florian Kinder <florian.kinder@fankserver.com>
2018-05-30 20:33:33 +0200

    Removed enviroment variable and added parameter add custom options

--- 0.16/files/docker-entrypoint.sh
+++ 0.16/files/docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -x
-
 set -e
 
 id
@@ -45,5 +44,5 @@ exec /opt/factorio/bin/x64/factorio \
   --server-banlist $CONFIG/server-banlist.json \
   --rcon-port $RCON_PORT \
   --rcon-password "$(cat $CONFIG/rconpw)" \
-  --latency $LATENCY \
-  --server-id /factorio/config/server-id.json
+  --server-id /factorio/config/server-id.json \
+  $@
