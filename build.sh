86d88a333c67a5688011d3f3c4e9e4d9278b7442
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-05-04 18:34:16 +0200

    Build short and long image tag

--- build.sh
+++ build.sh
@@ -2,19 +2,25 @@
 set -eo pipefail
 
 if [ -z "$1" ]; then
-  echo "Usage: ./build.sh \$VERSION"
+  echo "Usage: ./build.sh \$VERSION_SHORT"
 else
-  VERSION="$1"
+  VERSION_SHORT="$1"
 fi
 
-cd "$VERSION" || exit
+VERSION=$(grep -oP '[0-9]+\.[0-9]+\.[0-9]+' "$VERSION_SHORT/Dockerfile" | head -1)
+cd "$VERSION_SHORT" || exit
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   if [ "$TRAVIS_BRANCH" == "master" ]; then
-    TAG="$VERSION"
+    TAG="$VERSION -t factoriotools/docker_factorio_server:$VERSION_SHORT"
   else
     TAG="$TRAVIS_BRANCH"
   fi
+
+  docker build . -t "factoriotools/docker_factorio_server:$TAG"
+
+  # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  # docker push "factoriotools/docker_factorio_server:$VERSION"
 fi
 
-docker build . -t "factoriotools/docker_factorio_server:$TAG"
+docker images
