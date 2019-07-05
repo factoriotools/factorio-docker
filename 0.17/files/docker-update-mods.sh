1eab764c5e3026e908f1c0dc56e02b0333dec674
Carl Bennett <carl@carlbennett.me>
2019-07-04 20:22:53 -0500

    Make POSIX compliant

--- 0.17/files/docker-update-mods.sh
+++ 0.17/files/docker-update-mods.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -x
 
-if [[ -f /run/secrets/username ]]; then
+if [ -f /run/secrets/username ]; then
   USERNAME=$(cat /run/secrets/username)
 fi
 
-if [[ -f /run/secrets/username ]]; then
+if [ -f /run/secrets/username ]; then
   TOKEN=$(cat /run/secrets/token)
 fi
 
-if [[ -z $TOKEN ]]; then
+if [ -z $TOKEN ]; then
   set -- "$(jq -j ".username, \" \", .token" "$CONFIG/server-settings.json")"
   USERNAME=$1
   TOKEN=$2
