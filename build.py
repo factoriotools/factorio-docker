2e9b062e1e2c3f04b83e7df4af1d435a1eb510d2
ruben <ruben.simons@web.de>
2021-03-05 23:08:41 +0100

    Include login in build script

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ import os
 import json
 import subprocess
 import shutil
+import sys
 import tempfile
 
 from jinja2 import Environment, FileSystemLoader
@@ -23,20 +24,48 @@ def build_dockerfile(sha1, version, tags):
     build_command = ["docker", "build", "."]
     for tag in tags:
         build_command.extend(["-t", f"factoriotools/factorio:{tag}"])
-    subprocess.run(build_command, cwd=build_dir)
+    try:
+        subprocess.run(build_command, cwd=build_dir, check=True)
+    except subprocess.CalledProcessError:
+        print("Build of image failed")
+        exit(1)
 
 
-def main():
+def login():
+    try:
+        username = os.environ["DOCKER_USERNAME"]
+        password = os.environ["DOCKER_PASSWORD"]
+        subprocess.run(["docker", "login", "-u", username, "-p", password], check=True)
+    except KeyError:
+        print("Username and password need to be given")
+        exit(1)
+    except subprocess.CalledProcessError:
+        print("Docker login failed")
+        exit(1)
+
+
+def main(push_tags=False):
     with open(os.path.join(os.path.dirname(__file__), "buildinfo.json")) as file_handle:
         builddata = json.load(file_handle)
+
+    if push_tags:
+        login()
 
     for version, buildinfo in builddata.items():
         sha1 = buildinfo["sha1"]
         tags = buildinfo["tags"]
         build_dockerfile(sha1, version, tags)
+        if not push_tags:
+            continue
         for tag in tags:
-            subprocess.run(["docker", "push", f"factoriotools/factorio:{tag}"])
+            try:
+                subprocess.run(["docker", "push", f"factoriotools/factorio:{tag}"],
+                               check=True)
+            except subprocess.CalledProcessError:
+                print("Docker push failed")
+                exit(1)
 
 
 if __name__ == '__main__':
-    main()
+    push_tags = len(sys.argv) > 1 and sys.argv[1] == "--push-tags"
+    main(push_tags)
